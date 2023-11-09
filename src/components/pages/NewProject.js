9c11b4f3d331e65b06881341e69db384ae2da6c5
Gustavo Ferreira <84362116+GustavoFerrera@users.noreply.github.com>
2023-11-09 15:12:38 -0300

    Update NewProject.js

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -21,7 +21,7 @@ function NewProject () {
         .then((data) => {
             console.log(data)
             // redirecionar
-            history('/', { state: { message: 'Projeto criado com sucesso!' }})
+            history('/projects', { state: { message: 'Projeto criado com sucesso!' }})
         })
         .catch((err) => console.log(err))
     }
